727a910e2cb40688e2ac7b55beac2027eef6160e
Julien Delvaux <delvaux.ju@gmail.com>
2018-07-08 11:25:38 +0200

    Add conversion from unix timestamp to rtc_ds3231m struct Clean all compilers warning

--- DS3231M_library/DS3231M_library/src/lib/DS3231M.c
+++ DS3231M_library/DS3231M_library/src/lib/DS3231M.c
@@ -39,7 +39,7 @@ void DS3231M_setTime(rtc_ds3231m *ds3231m){
 		bin2bcd(ds3231m->second),
 		bin2bcd(ds3231m->minute),
 		bin2bcd(ds3231m->hour),
-		bin2bcd(ds3231m->day),
+		bin2bcd(ds3231m->day_of_week),
 		bin2bcd(ds3231m->date),
 		bin2bcd(ds3231m->month),
 		bin2bcd(ds3231m->year - 2000)} ;
@@ -103,11 +103,99 @@ void DS3231M_getTime(rtc_ds3231m *ds3231m){
 	ds3231m->second = bcd2bin(buffer[DS3231_REGISTER_SECONDS]);
 	ds3231m->minute = bcd2bin(buffer[DS3231_REGISTER_MINUTES]);
 	ds3231m->hour = bcd2bin(buffer[DS3231_REGISTER_HOUR]);
-	ds3231m->day = bcd2bin(buffer[DS3231_REGISTER_DAY]);
+	ds3231m->day_of_week = bcd2bin(buffer[DS3231_REGISTER_DAY]);
 	ds3231m->date = bcd2bin(buffer[DS3231_REGISTER_DATE]);
 	ds3231m->month = bcd2bin(buffer[DS3231_REGISTER_MONTH]);
 	ds3231m->year = bcd2bin(buffer[DS3231_REGISTER_YEAR]);
 		
+}
+
+// 
+
+uint64_t convert_dateTime_to_unixms(rtc_ds3231m *ds3231m){
+	uint16_t y;
+    uint8_t m;
+    uint8_t d;
+    uint64_t unix_timestamp;
+ 
+    // Year
+    y = ds3231m->year;
+    // Month of year
+    m = ds3231m->month;
+    // Day of month
+    d = ds3231m->date;
+ 
+    // January and February are counted as months 13 and 14 of the previous year
+    if(m <= 2)
+    {
+       m += 12;
+       y -= 1;
+    }
+ 
+    // Convert years to days
+    unix_timestamp = (365 * y) + (y / 4) - (y / 100) + (y / 400);
+    // Convert months to days
+    unix_timestamp += (30 * m) + (3 * (m + 1) / 5) + d;
+    // Unix time starts on January 1st, 1970
+    unix_timestamp -= 719561;
+    // Convert days to seconds
+    unix_timestamp *= 86400;
+    // Add hours, minutes and seconds
+    unix_timestamp += (3600 * ds3231m->hour) + (60 * ds3231m->minute) + ds3231m->second;
+	
+	// Convert to ms
+	unix_timestamp = unix_timestamp*1000;
+ 
+    // Return Unix time
+  return unix_timestamp;
+}
+
+void convert_unixms_to_dateTime(uint64_t unix_timestamp_ms, rtc_ds3231m *ds3231m){
+	uint32_t a;
+	uint32_t b;
+	uint32_t c;
+	uint32_t d;
+	uint32_t e;
+	uint32_t f;
+	    
+	//Negative Unix time values are not supported
+	if(unix_timestamp_ms < 1){
+		unix_timestamp_ms = 0;
+	}
+	    
+	//Retrieve hours, minutes and seconds
+	uint64_t unix_timestamp = unix_timestamp_ms /1000;
+	ds3231m->second = unix_timestamp % 60;
+	unix_timestamp /= 60;
+	ds3231m->minute = unix_timestamp % 60;
+	unix_timestamp /= 60;
+	ds3231m->hour = unix_timestamp % 24;
+	unix_timestamp /= 24;
+	    
+	//Convert Unix time to date
+	a = (uint32_t) ((4 * unix_timestamp + 102032) / 146097 + 15);
+	b = (uint32_t) (unix_timestamp + 2442113 + a - (a / 4));
+	c = (20 * b - 2442) / 7305;
+	d = b - 365 * c - (c / 4);
+	e = d * 1000 / 30601;
+	f = d - e * 30 - e * 601 / 1000;
+	    
+	//January and February are counted as months 13 and 14 of the previous year
+	if(e <= 13)
+	{
+		c -= 4716;
+		e -= 1;
+	}
+	else
+	{
+		c -= 4715;
+		e -= 13;
+	}
+	    
+	//Retrieve year, month and day
+	ds3231m->year = c;
+	ds3231m->month = e;
+	ds3231m->date = f;
 }
 
 
--- DS3231M_library/DS3231M_library/src/lib/DS3231M.h
+++ DS3231M_library/DS3231M_library/src/lib/DS3231M.h
@@ -28,19 +28,16 @@
 */
 #define DS3231_DEFAULT_ADDRESS 0x68
 
-typedef struct rtc_struct {
+typedef struct rtc_ds3231m_struct {
 	uint8_t address;
-	uint32_t year;
-	uint8_t month;
-	uint8_t day;
-	uint8_t date;
-	uint8_t hour;
-	uint8_t minute;
-	uint8_t second;
-
-} rtc_struct_t;
-
-typedef rtc_struct_t rtc_ds3231m;
+	uint16_t year; // Year
+	uint8_t month; // Month
+	uint8_t day_of_week; // Day Of The Week
+	uint8_t date; // Date of day in the month
+	uint8_t hour; // Hour
+	uint8_t minute; //Minute 
+	uint8_t second; //Second
+} rtc_ds3231m;
  
 /*
    ---------------------------------------
@@ -51,5 +48,8 @@ typedef rtc_struct_t rtc_ds3231m;
 extern void DS3231M_init(uint8_t address);
 extern void DS3231M_setTime(rtc_ds3231m *ds3231m);
 extern void DS3231M_getTime(rtc_ds3231m *ds3231m);
+
+extern uint64_t convert_dateTime_to_unixms(rtc_ds3231m *ds3231m);
+extern void convert_unixms_to_dateTime(uint64_t unix_timestamp_ms, rtc_ds3231m *ds3231m);
 
 #endif /* DS3231M_H_ */
